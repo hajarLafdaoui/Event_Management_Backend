fd11709ac524f54cdd90d0ea22d5fbd6d2a7ec27
ismailouab <ismailouabderrazak@gmail.com>
2025-05-03 00:44:36 +0100

    Save local changes to api.php before pulling

--- routes/api.php
+++ routes/api.php
@@ -56,13 +56,14 @@ Route::prefix('auth')->group(function () {    // Basic Authentication
         ->name('verification.send');
 });
 
-//--- EVENT MANAGEMENT ROUTES ---//
-
 
 // A standalone example if you ever need a “/user” endpoint:
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+//--- EVENT MANAGEMENT ROUTES ---//
 // Event Types Routes
 Route::prefix('event-types')->middleware('auth:api')->group(function () {
     Route::get('/', [EventTypeController::class, 'index']);
